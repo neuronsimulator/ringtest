74484c6591813305575b406f9c815107293c39c3
Michael Hines <michael.hines@yale.edu>
2016-05-19 09:25:52 +0200

    arg syntax problem

--- ringtest.py
+++ ringtest.py
@@ -5,8 +5,8 @@ parser.add_argument("-mpi", action='store_true')
 parser.add_argument("-nring", metavar='N', help="number of rings (default 16)", type=int, default=16)
 parser.add_argument("-ncell", metavar='N', help="number of cells per ring (default 8)", type=int, default=8)
 parser.add_argument('-npt', metavar='N', help="number of cells per type (default 8)", type=int, default=8)
-parser.add_argument("-branch", metavar='[N,N]', help="range of branches per cell (default [10,20])", type=list, default=[10,20])
-parser.add_argument("-compart", metavar='[N,N]', help="range of compartments per branch (default [1,1])", type=list, default=[1,1])
+parser.add_argument("-branch", metavar='N', nargs=2, help="range of branches per cell (default 10 20)", type=int, default=[10,20])
+parser.add_argument("-compart", metavar='N', nargs=2, help="range of compartments per branch (default [1,1])", type=int, default=[1,1])
 parser.add_argument("-tstop", metavar='float', help="stop time (ms) (default 100.0)", type=float, default=100.)
 parser.add_argument("-gran", metavar='N', help="global Random123 index (default 0)", type=int, default=0)
 parser.add_argument("-rparm", dest='rparm', action='store_true', help="randomize parameters", default=False)
@@ -21,6 +21,7 @@ ncell_per_type = args.npt
 
 nbranch=args.branch # min, max random number of dend sections (random tree topology)
 ncompart=args.compart # min, max random nseg for each branch
+print nbranch, ncompart
 
 # number of distinct cell types (same branching and compartments)
 #each cell has random type [0:ntype]
