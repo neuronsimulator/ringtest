015d6c12723d44886d9fcf8868f6d0bea7b58d42
Michael Hines <michael.hines@yale.edu>
2021-02-07 17:13:38 -0500

    Much simplified by using the latest pc.nrncore_write(path, append).

--- test_submodel.py
+++ test_submodel.py
@@ -7,9 +7,9 @@
 #    mpiexec -n 4 nrniv -python -mpi -rparm -coreneuron -filemode ringtest.py
 #    diff -r coredat corenrn_data
 # Note: -rparm is used for a more substantive test in that all the cells are
-# distinct with differt parameters.
+# distinct with different parameters.
 
-# The overall strategy for a ringtest model relies on the fact that it is
+# The overall strategy for a ringtest model relies on the fact that it
 # already does parallel setup so that there are pc.nhost() submodels,
 # one built on each rank, pc.id(). This setup did not use pc.nhost() or
 # pc.id() directly but stored those values in the global variables nhost and
@@ -32,18 +32,17 @@ import ringtest
 def test_submodel(nsubmodel):
   coredat = "./coredat"
   cvode.cache_efficient(1)
-  gidgroups = [h.Vector() for _ in range(nsubmodel)] # used to write files.dat at end
   for isubmodel in range(nsubmodel):
     submodel = build_submodel(isubmodel, nsubmodel) # just like a single rank on an nhost cluster
-    pc.nrnbbcore_write("./coredat",  gidgroups[isubmodel])
+    # second arg below when False, means to begin a new files.dat file.
+    #  when True, means to append the groupid information to the existing file.
+    pc.nrncore_write("./coredat",  isubmodel != 0)
     teardown()
     submodel = None
 
     # verify no netcons or sections. Ready to go on to next submodel
     assert (h.List("NetCon").count() == 0)
     assert (len([s for s in h.allsec()]) == 0)
-
-  write_files_dat(coredat, gidgroups)
 
 def build_submodel(isubmodel, nsubmodel):
   # fake nhost and rank
@@ -66,20 +65,6 @@ def teardown():
   # delete your Cells list
   # unfortunately, cannot delete submodel here as there is reference to it
   # in test_submodel(nsubmodel)
-
-# write out the files.dat file
-def write_files_dat(coredat, gidgroups):
-  f = open(coredat+"/files.dat", "w")
-  f.write("1.4\n") # CoreNEURON data version
-
-  ng = sum(len(g) for g in gidgroups)
-  f.write(str(ng) + '\n') # number of groups
-
-  for gidgroup in gidgroups:
-    for x in gidgroup:
-      f.write(str(int(x)) + '\n') # group id
-
-  f.close()
 
 if __name__ == "__main__":
   test_submodel(4)
