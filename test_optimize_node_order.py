71630fd8b4ffcaf77992847182db17b605118ae1
Michael Hines <michael.hines@yale.edu>
2024-03-05 20:23:41 -0500

    factor ringtest.py a bit so it can be imported by other tests.

---
+++ test_optimize_node_order.py
@@ -0,0 +1,26 @@
+from neuron import h
+import ringtest as rt
+
+pc = rt.pc
+
+
+def tst_optimize_node_order():
+    model = rt.create_rings()
+
+    from commonutils import tvec, idvec
+
+    spikes = []
+    for i in range(3):
+        pc.optimize_node_order(i)
+        h.stdinit()
+        rt.runsim()
+        spikes.append((tvec.c(), idvec.c()))
+    print("nspike ", spikes[0][0].size())
+    for i in range(1, 3):
+        for j in range(2):
+            assert spikes[i][j].eq(spikes[0][j])
+    pc.optimize_node_order(0)
+
+
+if __name__ == "__main__":
+    tst_optimize_node_order()
